0be1fc0ab7387acccf1e63f7099e0d572e25e039
Makarov Alexandr <makarov_ai@sibsau.ru>
2021-11-10 13:37:30 +0700

    bigindian test

---
+++ c_pointer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define uchar unsigned char
+
+int main(void)
+{
+    int a = 25963548;
+    uchar c = 255;
+    uchar d = 254;
+    uchar e = 253;
+    uchar f = 252;
+    uchar* ptr = (uchar*)&a;
+
+    printf("1 -> %x\n", *ptr);
+    ptr++;
+    
+    printf("2 -> %x\n", *ptr);
+    ptr++;
+    
+    printf("3 -> %x\n", *ptr);
+    ptr++;
+    
+    printf("4 -> %x\n", *ptr);
+    ptr++;
+ 
+    printf("5 -> %x\n", *ptr);
+
+    printf("char c = %x\n", c);
+}
