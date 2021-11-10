dd42a2fad61b52d59ea9e9f4e5258fb02c5e7995
Makarov Alexandr <makarov_ai@sibsau.ru>
2021-11-10 10:54:20 +0700

    add 3.c

---
+++ lWork1/3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//Output elements of array
+void ShowArr(int arr[], int N)
+{
+    for(int i=0; i<N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    puts("");
+}
+
+int main(int argc, char* argv[])
+{
+    int N, tmp;
+    
+    do
+    {
+        getchar(); getchar();
+        printf("Enter the N: ");
+    }while(scanf("%d", &N)!=1);
+
+
+    //fill array
+    //for(int i=0; i<N; i++) { arr[i]=i+1; }
+ 
+    //printf("\e[38;5;40m initial array -> "); ShowArr(arr, N);
+
+    //reverse array
+    //for(int i=0; i < N/2; i++)
+    ///{
+     //   tmp = arr[i];
+       // arr[i]=arr[N-i-1];
+       // arr[N-i-1]=tmp;
+    //}
+
+    //printf("\e[38;5;51mreversed array -> "); ShowArr(arr, N);
+    //printf("\e[38;5;255m");
+    return 0;
+}
