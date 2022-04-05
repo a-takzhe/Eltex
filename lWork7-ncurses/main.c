d82c67ce98acadb616bb8d8347938c04ecc97559
Alexandr <makarov_ai@sibsau.ru>
2022-04-05 16:41:27 +0700

    commit from work: start of the programm

--- lWork7-ncurses/main.c
+++ lWork7-ncurses/main.c
@@ -4,6 +4,29 @@
 #include <stdlib.h>
 #include <curses.h>
 
+WINDOW *mainwnd;
+WINDOW *cwnd;
+WINDOW *subwnd;
+
+struct command{
+    char* key;
+    char* description;
+};
+
+enum COLOR_SHEMA
+{
+    MAINWND=1,
+    CWND=2
+};
+
+void init_color_pairs()
+{
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLUE);
+    init_pair(2, COLOR_WHITE, COLOR_BLUE);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+}
+
 void sig_winch(int signo)
 {
     struct winsize size;
@@ -18,36 +41,64 @@ void sig_winch(int signo)
 
 int main(int argc, char ** argv)
 {
-    WINDOW * wnd;
-    WINDOW * subwnd;
     initscr();
     signal(SIGWINCH, sig_winch);
     cbreak();
-    curs_set(0);
-    refresh();
 
     struct winsize size;
     ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size);
-
-    wnd = newwin(3, size.ws_col, size.ws_row-3, 0);
-    box(wnd, '|', '-');
-    //subwnd = derwin(wnd, 4, 16, 1, 1);
-    // wprintw(subwnd, "Hello, brave new curses world!\n");
-    wmove(wnd, 1,1);
-
-    start_color();
-    init_pair(1, COLOR_CYAN, COLOR_YELLOW);
-    wattron(wnd, COLOR_PAIR(1));
-    wprintw(wnd, "F1 - open file");
-    wrefresh(wnd);
-    //delwin(subwnd);
-    delwin(wnd);
-    wattroff(wnd, COLOR_PAIR(1));
+    init_color_pairs();
+    // mainwnd = newwin(5, 100, 5, 1);
     
-    move(0, 0);
+    // box(mainwnd, '.', '.');
+    // box(cwnd, '.', '.');
+    
+    // wbkgd(mainwnd, COLOR_PAIR(1) | A_BOLD);
+    
+    bkgd(COLOR_PAIR(3) | A_BOLD);
+    refresh();
+    // wrefresh(mainwnd);
+    
+    
     printw("Press any key to continue...");
     refresh();
+    
+    cwnd = newwin(2, 100, size.ws_row-2, 0);
+    wmove(cwnd, 0, 0);
+    wprintw(cwnd, "F1");
+    wbkgd(cwnd, COLOR_PAIR(2));
+    wrefresh(cwnd);
+
+    subwnd = derwin(cwnd, 1, 10, 0, 3);
+    wmove(subwnd, 0, 0);
+    wprintw(subwnd, "open file");
+    wbkgd(subwnd, COLOR_PAIR(1)|A_BOLD);
+    wrefresh(subwnd);
+    wrefresh(cwnd);
+    
     getch();
+
+    // wmove(mainwnd, 1, 1);
+    // //wrefresh(mainwnd);
+    // getch();
+
+    // wmove(cwnd, 1, 1);
+    // wrefresh(cwnd);
+    // getch();
+    // wnd = newwin(3, size.ws_col, size.ws_row-3, 0);
+    // box(wnd, '|', '-');
+    // //subwnd = derwin(wnd, 4, 16, 1, 1);
+    // // wprintw(subwnd, "Hello, brave new curses world!\n");
+    // wmove(wnd, 1,1);
+
+    // wprintw(wnd, "F1 - open file");
+    // wrefresh(wnd);
+    // //delwin(subwnd);
+    // delwin(wnd);
+    // wattroff(wnd, COLOR_PAIR(1)|A_BOLD);
+    
+    delwin(mainwnd);
+    delwin(cwnd);
     endwin();
     exit(EXIT_SUCCESS);
 }
