c1fe452fc7fbfbb052916fb3af3201a4088c999a
Alexandr <makarov_ai@sibsau.ru>
2022-06-23 14:16:38 +0700

    lWork9 - execve finish

--- lWork9-mybash/lib/execve.h
+++ lWork9-mybash/lib/execve.h
@@ -2,7 +2,13 @@
 #define EXECVE_H_
 
 #include "strok.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h> 
+#include <unistd.h>
 
-int exec(char** str);
+
+int exec(char* str);
 
 #endif
