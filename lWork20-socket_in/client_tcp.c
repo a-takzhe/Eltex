88443a27d2657868b76f7d256eeb7938c1b4a860
Makarov Alexandr <makarov.ai.dev@gmail.com>
2022-10-31 20:47:03 +0700

    test on work

--- lWork20-socket_in/client_tcp.c
+++ lWork20-socket_in/client_tcp.c
@@ -7,15 +7,41 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int FD; 
+#define client_exit(fd, func_name) \
+        do{perror(func_name); close(fd); exit(EXIT_FAILURE);}while(0)
 
 int main (int argc, char *argv[])
 {
     struct sockaddr_in serv_addr, client_addr;
+    int fd;
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s host port\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        client_exit(-1, "socket");
     }
 
+    memset(&client_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.2.2");
+    serv_addr.sin_port = htons(atoi("8080"));
+
+    // if(setsockopt(fd, SOL_IP, IP_FREEBIND, &(int){1}, sizeof(int)) == -1){
+    //     client_exit(fd, "setsockopt");
+    // }
+    if(bind(fd, (struct sockaddr*)&client_addr, sizeof(struct sockaddr)) == -1){
+        client_exit(fd, "bind");
+    }
+
+    printf("client ip: %s\n", inet_ntoa(client_addr.sin_addr));
+
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = atoi(argv[2]);
+
+    int len = sizeof(struct sockaddr_in);
+    if(connect(fd, (const struct sockaddr*)&serv_addr, len) == -1){
+        client_exit(fd, "connect");
+    }
+
+    printf("client ip: %s\n", inet_ntoa(client_addr.sin_addr));
 }
--- lWork20-socket_in/server_tcp.c
+++ lWork20-socket_in/server_tcp.c
@@ -15,27 +15,29 @@ int FD;
 
 int main (int argc, char *argv[])
 {
+    int client_fd;
     struct sockaddr_in serv_addr, client_addr;
 
-    struct hostent *he = gethostbyname("serv1");
-    if(he == NULL){
-        perror("gethostbyname");
-    }
-    printf("my int ip: %d\n", inet_addr("127.0.1.5"));
-    printf("ip1: %d\n", ntohl((*((struct in_addr*)he->h_addr_list[0])).s_addr));
-    printf("ip: %s\n", inet_ntoa(*((struct in_addr*)he->h_addr_list[0])));
+    // struct hostent *he = gethostbyname("serv1");
+    // if(he == NULL){
+    //     perror("gethostbyname");
+    // }
+    // printf("my int ip: %d\n", inet_addr("127.0.1.5"));
+    // printf("ip1: %d\n", ntohl((*((struct in_addr*)he->h_addr_list[0])).s_addr));
+    // printf("ip: %s\n", inet_ntoa(*((struct in_addr*)he->h_addr_list[0])));
 
-    struct in_addr ip_addr;
-    ip_addr.s_addr = ntohl((*((struct in_addr*)he->h_addr_list[0])).s_addr);
-    printf("revert ip: %s\n", inet_ntoa(ip_addr));
+    // struct in_addr ip_addr;
+    // ip_addr.s_addr = ntohl((*((struct in_addr*)he->h_addr_list[0])).s_addr);
+    // printf("revert ip: %s\n", inet_ntoa(ip_addr));
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s host(127.*.*.*) port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    FD = socket(AF_INET, SOCK_STREAM, 0);
-    
+    if((FD = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        serv_exit(-1, "socket");
+    }
     
     memset(&serv_addr, 0, sizeof(struct sockaddr_in));
     serv_addr.sin_family = AF_INET;
@@ -45,8 +47,25 @@ int main (int argc, char *argv[])
     if(bind(FD, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in)) == -1){
         serv_exit(FD, "bind");
     }
+    if(listen(FD, 5) == -1){
+        serv_exit(FD, "listen");
+    }
+
+    socklen_t len = sizeof(client_addr); 
+    if((client_fd = accept(FD, (struct sockaddr*)0, (int)0)) == -1){
+        serv_exit(FD, "accept");
+    }
+        printf("server ip: %s\n", inet_ntoa(serv_addr.sin_addr));
 
 
+    if(getpeername(client_fd, (struct sockaddr*)&client_addr, &len) == -1){
+        serv_exit(FD, "getsockname");
+    }
+    // if(getsockname(client_fd, (struct sockaddr*)&client_addr, &len) == -1){
+    //     serv_exit(FD, "getsockname");
+    // }
+    printf("client ip: %s\n", inet_ntoa(client_addr.sin_addr));
+    printf("client port: %d\n", client_addr.sin_port);
     
     close(FD);
 }
