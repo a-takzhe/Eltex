4639132525ec8dde68f2281768c3fc74a7409c51
Alexandr Makarov <makarov.ai.dev@gmail.com>
2022-11-09 20:24:44 +0700

    lWork-20 complete

--- lWork20-socket_in/client_udp.c
+++ lWork20-socket_in/client_udp.c
@@ -1,3 +1,4 @@
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,32 +46,18 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE); 
     }
 
-    if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
         client_exit(-1, "socket");
     }
-
-    //printf("desired client addr: 192.168.123.1:%d\n", atoi("8888"));
 
     memset(&client_addr, 0, sizeof(struct sockaddr_in));
     client_addr.sin_family = AF_INET;
     client_addr.sin_addr.s_addr = inet_addr(hip);
     client_addr.sin_port = htons(hport);
 
-    // if(setsockopt(fd, SOL_IP, IP_FREEBIND, &(int){1}, sizeof(int)) == -1){
-    //     client_exit(fd, "IP_FREEBIND");
-    // }
-    // if(setsockopt(fd, SOL_IP, IP_TRANSPARENT, &(int){1}, sizeof(int)) == -1){
-    //     client_exit(fd, "IP_TRANSPARENT");
-    // }
-    // if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR|SO_REUSEPORT, &(int){1}, sizeof(int)) == -1){
-    //     client_exit(fd, "SO_REUSEADDR|SO_REUSEPORT");
-    // }
-
     if(bind(fd, (struct sockaddr*)&client_addr, sizeof(client_addr)) == -1){
         client_exit(fd, "bind");
     }
-
-    printf("my addr after bind: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
     memset(&serv_addr, 0, sizeof(struct sockaddr_in));
     serv_addr.sin_family = AF_INET;
@@ -78,19 +65,14 @@ int main (int argc, char *argv[])
     serv_addr.sin_port = htons(sport);
 
     puts("Connected to server...");
-    int len = sizeof(client_addr);
+    int len = sizeof(serv_addr);
     if(connect(fd, (const struct sockaddr*)&serv_addr, len) == -1){
         client_exit(fd, "connect");
     }
 
-    if(getsockname(fd, (struct sockaddr*)&client_addr, &len) == -1){
-        client_exit(fd, "getsockname");
-    }
-    printf("client addr after connect: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
     char buf[256];
-    strcpy(buf, "Hi server!\n\tThat`s all, you can`t have not loopback address!");
-    printf("send to server(%s:%d)>> %s\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port), buf);
+    strcpy(buf, "Hi server! This is UDP DGRAM!");
+    printf("sendto server(%s:%d)>> %s\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port), buf);
     if(send(fd, buf, 256, 0) == -1){
         client_exit(fd, "send");
     }
--- lWork20-socket_in/server_udp.c
+++ lWork20-socket_in/server_udp.c
@@ -8,9 +8,73 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int fd;
+int FD; 
+
+#define serv_exit(fd, func_name) \
+        do{perror(func_name); close(fd); exit(EXIT_FAILURE);}while(0)
 
 int main(int argc, char** argv)
 {
+    int opt, client_fd;
+    char host[16] = {0};
+    int port = -1;
+    struct sockaddr_in serv_addr, client_addr;
+
+    while ((opt = getopt(argc, argv, "h:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            printf("server address: %s\n", optarg);
+            char* ind = index(optarg, (int)':');
+            if (ind == NULL){port = -1; break;}
+            strncpy(host, optarg, (int)(ind-optarg));
+            port = atoi(optarg+(ind-optarg)+1);
+            break;
+
+        default:
+            break;
+        }
+    }
+    if(port == -1){
+        fprintf(stderr, "Usage: %s -h 127.*.*.*:port\n", argv[0]);
+        exit(EXIT_FAILURE); 
+    }
+
+    if((FD = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        serv_exit(-1, "socket");
+    }
     
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(host);
+    serv_addr.sin_port = htons(port);
+    
+    if(bind(FD, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in)) == -1){
+        serv_exit(FD, "bind");
+    }
+    printf("server (%s:%d) binding successful!\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port));
+
+
+    char buf[256];
+    socklen_t len = sizeof(client_addr); 
+    if(recvfrom(FD, buf, 256, 0, (struct sockaddr*)&client_addr, &len) == -1){
+        serv_exit(FD, "recvfrom");
+    }
+    // if(getpeername(client_fd, (struct sockaddr*)&client_addr, &len) == -1){
+    //     serv_exit(FD, "getsockname");
+    // }
+    printf("client (%s:%d) send msg >> %s!\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf);
+
+
+    memset(buf, 0, 256);
+    strcpy(buf, "Recvfom client successe!");
+    if(sendto(FD, buf, 256, 0, (struct sockaddr*)&client_addr, len) == -1){
+        serv_exit(FD, "sendto");
+    }
+
+    puts("BAY, server close all desriptors!");
+    close(FD);
+    close(client_fd);
+
 }
