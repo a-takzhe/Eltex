733e3e502c1d45a7a89cd6050598cc67c7d9f48f
Alexandr Makarov <makarov.ai.dev@gmail.com>
2022-12-06 21:42:25 +0700

    serial server accept

--- lWork21-server_schemes/serial/client/src/client.c
+++ lWork21-server_schemes/serial/client/src/client.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <time.h>
 
-int main(){
-    puts("hallo world client!");
+
+#define client_exit(fd, func_name) \
+        do{perror(func_name); close(fd); exit(EXIT_FAILURE);}while(0)
+
+
+int main (int argc, char *argv[])
+{
+    int fd;
+    char buf[256];
+    //socklen_t len = sizeof(struct sockaddr_in);
+    struct sockaddr_in addr, dst_addr;
+    
+    if((fd = socket(AF_INET, SOCK_STREAM, 0)) ==-1){
+        client_exit(fd, "socket");
+    }
+    puts("socket init: OK!");
+
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family      = AF_INET;
+    addr.sin_port        = htons(8001);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    bind(fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
+
+    memset(&dst_addr, 0, sizeof(struct sockaddr_in));
+    dst_addr.sin_family      = AF_INET;
+    dst_addr.sin_port        = htons(8000);
+    dst_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(connect(fd, (struct sockaddr*)&dst_addr, sizeof(struct sockaddr_in)) == -1){
+        client_exit(fd, "bind");
+    }
+    
+    memset(buf, 0, 256);
+    if(recv(fd, buf, 256, 0)==-1){
+        client_exit(fd, "recv");
+    }
+    printf("server send >>  %s\n", buf);
+    
+    close(fd);
 }
--- lWork21-server_schemes/serial/server/src/server.c
+++ lWork21-server_schemes/serial/server/src/server.c
@@ -5,11 +5,27 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define serv_exit(fd, func_name) \
         do{perror(func_name); close(fd); exit(EXIT_FAILURE);}while(0)
 
+void currentTime(char * const buf)
+{
+    time_t timep = time(NULL);
+    struct tm* ltime = localtime(&timep);
+    if (ltime == NULL) {
+        perror("localtime");
+        exit(EXIT_FAILURE);
+    }
 
+    memset(buf, 0, 256);
+
+    if(strftime(buf, 256, "%d-%m-%Y %H:%M:%S", ltime) == 0){
+        perror("strftime");
+        exit(EXIT_FAILURE);
+    };
+}
 
 int main (int argc, char *argv[])
 {
@@ -17,6 +33,7 @@ int main (int argc, char *argv[])
     char buf[256];
     socklen_t len = sizeof(struct sockaddr_in);
     struct sockaddr_in addr, dst_addr;
+    
 
     if((fd = socket(AF_INET, SOCK_STREAM, 0)) ==-1){
         serv_exit(fd, "socket");
@@ -36,16 +53,25 @@ int main (int argc, char *argv[])
     if(listen(fd, 5) == -1){
         serv_exit(fd, "listen");
     }
+    puts("server vait aconnection...");
 
     while(1)
     {    
+        memset(&dst_addr, 0, sizeof(struct sockaddr_in));
         if((dst_fd = accept(fd, (struct sockaddr*)&dst_addr, &len)) == -1){
             serv_exit(fd, "accept");
         }
-        //puts("")
 
-        if(send(dst_fd, buf, sizeof(buf), 0) == -1){
-
+        if(getpeername(dst_fd, (struct sockaddr*)&dst_addr, &len) == -1){
+            serv_exit(fd, "getsockname");
         }
+        printf("client <%s:%d> accepted!\n", inet_ntoa(dst_addr.sin_addr), ntohs(dst_addr.sin_port));
+        
+        currentTime(buf);
+        if(send(dst_fd, buf, sizeof(buf), 0) == -1){
+            serv_exit(fd, "send");
+        }
+
+        close(dst_fd);
     }
 }
